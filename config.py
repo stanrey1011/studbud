5d1626fce235b46ed2ef2ceb356589ac922f4df6
as <albert@stanrey.com>
2025-08-28 16:45:07 -1000

    Explicitly set SQLite URI in config.py for lightweight DB

--- config.py
+++ config.py
@@ -2,9 +2,10 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'studbud.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'studbud.db')  # Explicit SQLite default
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'uploads'
